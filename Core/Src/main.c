7dd24e5f1f314c19590e9f21d04148bb5ce0ff2c
Khang <55547021+Achicken7301@users.noreply.github.com>
2022-10-06 09:43:22 +0700

    update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -210,11 +210,25 @@ int main(void)
   int index_led = 0;
   int state = 0;
   int hour = 15, minute = 8, second = 50;
-//  setTimer0(1000);
+  setTimer0(1000);
   while (1) {
-	  if( timer0_flag == 1){
-		  HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin );
-		  setTimer0 (2000) ;
+	  second++;
+	  	if(second >= 60){
+	  		second = 0;
+	  		minute++;
+	  	}
+	  	if(minute >= 60){
+	  		minute = 0;
+	  		hour++;
+	  	}
+	  	if(hour >= 24){
+	  		hour = 0;
+	  	}
+	  	updateClockBuffer(hour, minute);
+
+	  if(timer0_flag == 1){
+		  setTimer0(500);
+		  HAL_GPIO_TogglePin(GPIOA, LED_RED_DOT_Pin);
 	  }
   }
 }
--- Core/Src/software_timer.c
+++ Core/Src/software_timer.c
@@ -13,7 +13,6 @@ int timer1_flag = 0;
 
 int timer2_counter = 0;
 int timer2_flag = 0;
-
 void setTimer1(int duration){
 	timer1_counter = duration;
 	timer1_flag = 0;
