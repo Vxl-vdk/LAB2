68f78c3359fb6f36ca114c4732f3611de5f78b97
Khang <55547021+Achicken7301@users.noreply.github.com>
2022-10-06 09:30:00 +0700

    update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -56,6 +56,19 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+int timer0_counter = 0;
+int timer0_flag = 0;
+int TIMER_CYCLE = 10;
+void setTimer0(int duration){
+	timer0_counter = duration / TIMER_CYCLE ;
+	timer0_flag = 0;
+}
+void timer_run(){
+	if( timer0_counter > 0){
+		timer0_counter --;
+		if( timer0_counter == 0) timer0_flag = 1;
+	}
+}
 
 /* USER CODE END 0 */
 
@@ -197,22 +210,12 @@ int main(void)
   int index_led = 0;
   int state = 0;
   int hour = 15, minute = 8, second = 50;
-  while (1)
-  {
-	second++;
-	if(second >= 60){
-		second = 0;
-		minute++;
-	}
-	if(minute >= 60){
-		minute = 0;
-		hour++;
-	}
-	if(hour >= 24){
-		hour = 0;
-	}
-	updateClockBuffer(hour, minute);
-	HAL_Delay(1000);
+//  setTimer0(1000);
+  while (1) {
+	  if( timer0_flag == 1){
+		  HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin );
+		  setTimer0 (2000) ;
+	  }
   }
 }
     /* USER CODE END WHILE */
@@ -345,6 +348,7 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 	void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef * htim){
+		timer_run ();
 		timer1Run();
 		timer2Run();
 	}
